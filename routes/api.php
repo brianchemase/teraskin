127140fed07ba6fedaf3b25e041567018fe3d1e6
Bayode Gabriel <bayogabriel24@gmail.com>
2024-09-09 17:46:32 +0000

    Sep 9, 2024, 6:46 PM

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TokenController;
+use App\Http\Controllers\FailedJobController;
+use App\Http\Controllers\PasswordResetTokenController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('users', UserController::class);
+
+Route::apiResource('personal-access-tokens', TokenController::class);
+
+Route::apiResource('failed-jobs', FailedJobController::class);
+
+Route::apiResource('password-reset-tokens', PasswordResetTokenController::class);
